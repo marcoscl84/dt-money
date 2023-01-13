21a5037621ffd8f98c37a4c8f04152aabcb001f1
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2023-01-13 11:02:18 -0300

    feat: inicio projeto

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      
+      <h1>Hello World</h1>
+    </ThemeProvider>
+    
+  )
+}
+
