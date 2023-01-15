c551fb6d7e286a86f79f6e5834de8e83fa1bda03
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2023-01-15 12:01:33 -0300

    feat: cálculo resumo de transações (summary)

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,6 +6,22 @@ import { SummaryCard, SummaryContainer } from "./styles";
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
 
+  // { income: 0, outcome: 0, total:0}
+  const summary = transactions.reduce(
+    (accumulator, transaction) => {
+      if (transaction.type === "income") {
+        accumulator.income += transaction.price;
+        accumulator.total += transaction.price;
+      } else {
+        accumulator.outcome += transaction.price;
+        accumulator.total -= transaction.price;
+      }
+
+      return accumulator;
+    },
+    { income: 0, outcome: 0, total: 0 }
+  );
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -15,7 +31,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>R$ {summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -25,7 +41,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>R$ {summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -35,7 +51,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>R$ {summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
