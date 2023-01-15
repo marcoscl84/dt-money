3da821d83b4bf2b14339ea4c4eb18b9e54013d52
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2023-01-15 16:50:57 -0300

    feat: formatação de valores

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -3,6 +3,7 @@ import { Header } from "../../components/Header";
 import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
 import { TransactionsContext } from "../../contexts/TransactionContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 import {
   PriceHighLight,
   TransactionsContainer,
@@ -52,11 +53,14 @@ export function Transactions() {
                   <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighLight variant={transaction.type}>
-                      {transaction.price}
+                      {transaction.type === "outcome" && "- "}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighLight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
                 </tr>
               );
             })}
