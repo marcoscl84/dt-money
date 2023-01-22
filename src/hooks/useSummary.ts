f3afd1682cc008e4497c45dc5018472e012ceff7
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2023-01-22 16:20:43 -0300

    feat: useMemo - aplicando a constantes

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { useContextSelector } from "use-context-selector";
 import { TransactionsContext } from "../contexts/TransactionContext";
+import { useMemo } from "react";
 
 export function useSummary() {
   const transactions = useContextSelector(TransactionsContext, (context: any) => {
@@ -8,20 +9,22 @@ export function useSummary() {
   });
 
   // { income: 0, outcome: 0, total:0}
-  const summary = transactions.reduce(
-    (accumulator: any, transaction: any) => {
-      if (transaction.type === "income") {
-        accumulator.income += transaction.price;
-        accumulator.total += transaction.price;
-      } else {
-        accumulator.outcome += transaction.price;
-        accumulator.total -= transaction.price;
-      }
-
-      return accumulator;
-    },
-    { income: 0, outcome: 0, total: 0 }
-  );
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (accumulator: any, transaction: any) => {
+        if (transaction.type === "income") {
+          accumulator.income += transaction.price;
+          accumulator.total += transaction.price;
+        } else {
+          accumulator.outcome += transaction.price;
+          accumulator.total -= transaction.price;
+        }
+  
+        return accumulator;
+      },
+      { income: 0, outcome: 0, total: 0 }
+    );
+  }, [transactions])
 
   return summary;
 }
